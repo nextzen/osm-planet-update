c814de58abdbbcd6644ca975d8afdf139c95dbe6
Julian Simioni <julian@mapzen.com>
2017-11-01 12:47:31 -0400

    Specify out file for osmupdate

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ cd /data
 rm planet-latest.osm.pbf
 wget http://planet.us-east-1.mapzen.com/planet-latest.osm.pbf
 
-osmupdate --day --hour planet-latest.osm.pbf
+osmupdate --day --hour planet-latest.osm.pbf planet-latest-updated.osm.pbf
